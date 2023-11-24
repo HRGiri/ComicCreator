c9861aea13729422c9d562bf65a9258abbf73c73
Himanshu Giri <himanshu.giri08@gmail.com>
2023-11-24 22:37:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ headers = {
 	"Content-Type": "application/json"
 }
 
-@st.cache_data
+# Fetch the generated image
+@st.cache_data     # Stored as cache for same query
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
 
+# Layout structure
 col_widths = {
         1: [[1]],
         2: [[1,1]],
@@ -44,17 +46,8 @@ st.title('Comic Creator')
 num_panels = st.number_input("How many panels do you want in your comic strip?", value=1, min_value=1, max_value=10)
 st.write('Building a comic strip of ', num_panels, ' panels')
 
-# num_cols = 2
-# rows = []
-# x = num_panels
-# while x > 0:
-#     if x > num_cols:
-#         x -= num_cols
-#         rows.append(num_cols)
-#     else:
-#         rows.append(x)
-#         x = 0
 
+# Create as many text_input fields as num_panels
 inputs = [''] * num_panels
 for num in range(num_panels):
     inputs[num] = st.text_input(f"Panel {num +1}", key=f"input{num}")    
@@ -62,20 +55,20 @@ for num in range(num_panels):
         inputs[num] += ", in comic style"
     print(inputs[num])
 
+# Fetch images from texts in the text_inputs and store in a list
 images = [None] * num_panels
 for num in range(num_panels):
     if inputs[num] != "":
         with st.spinner('Wait for it...'):
             image_bytes = query({
              	"inputs": inputs[num],
-            })
-            # images[num] = image_bytes
-            images[num] = Image.open(io.BytesIO(image_bytes))
-            # images[num] = image_bytes
+            })            
+            images[num] = Image.open(io.BytesIO(image_bytes))            
 
 if None not in images:
     st.write("Here's your comic strip...")
 
+# Display the images according to the layout
 with st.container():
     index = 0
     for r, row in enumerate(col_widths[num_panels]):
@@ -84,13 +77,13 @@ with st.container():
         for c, col in enumerate(cols):
             with col:
                 if contains_space and row[c] == 1:
-                    continue
+                    continue    # Empty for filling out space
                 if images[index] is not None:
                     st.image(images[index])
                     index += 1
 
 if None not in images:
-    # Construct a single image
+    # Construct a single image for download
     row_cols = col_widths[num_panels]
     num_rows = len(row_cols)
     num_cols = min([len(cols) for cols in row_cols])
@@ -113,30 +106,30 @@ if None not in images:
                 left = right + hpadding
                 continue
             if images[index] is not None:
-                right = left + 512                
-                # image = Image.open(io.BytesIO(images[index]))
+                right = left + 512                                
                 full_image.paste(images[index], (left, upper, right, lower))                
                 index += 1
                 left = right + hpadding
     # Add title
-    title = st.text_input(f"Add a title", key=f"title")
+    # title = st.text_input(f"Add a title", key=f"title")
     
     fp = io.BytesIO()
     full_image.save(fp, 'jpeg')
     data = fp.getvalue()
-    if title != '':
-        new_image = Image.new('RGB', (width, height + 100), color=(255,255,255))
-        new_image.paste(full_image,(0,100))
-        # Call draw Method to add 2D graphics in an image
-        I1 = ImageDraw.Draw(new_image)
-        # Custom font style and font size
-        # myFont = ImageFont.truetype('serif', 65)
-        # Add Text to an image
-        I1.text((30,30), title, fill=(0, 0, 0))
-        fp = io.BytesIO()
-        new_image.save(fp, 'jpeg')
-        data = fp.getvalue()
-        
+    # if title != '':
+    #     new_image = Image.new('RGB', (width, height + 100), color=(255,255,255))
+    #     new_image.paste(full_image,(0,100))
+    #     # Call draw Method to add 2D graphics in an image
+    #     I1 = ImageDraw.Draw(new_image)
+    #     # Custom font style and font size
+    #     # myFont = ImageFont.truetype('serif', 65)
+    #     # Add Text to an image
+    #     I1.text((30,30), title, fill=(0, 0, 0))
+    #     fp = io.BytesIO()
+    #     new_image.save(fp, 'jpeg')
+    #     data = fp.getvalue()
+    
+    # Finally download the file
     dowload = st.download_button(
         label="Download your comic",
         data=data,
